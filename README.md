1df445e0328d8971f4d99b230687ed1149856bbc
arsummers <aliyasummers1@gmail.com>
2020-02-14 17:26:22 -0800

    starting models, genre simple but likely finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # local-library-django-tutorial
+
+Using [MDN's Django tutorial](https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Tutorial_local_library_website) to stretch my Django skills and stay in practice. This may end up being what I use to keep track of my books. 
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    """model representing a book's genre"""
+    name = models.CharField(max_length=200, help_text='Enter a book genre (sci fi, kid lit, comics, etc)')
+
+    def __str__(self):
+        return self.name
+
+class Book:
+    pass
+
+class Author:
+    pass
+
