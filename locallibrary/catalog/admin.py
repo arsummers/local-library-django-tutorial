f4212889ab73abec0da2a0d77ad29784a4382cff
arsummers <aliyasummers1@gmail.com>
2020-02-24 14:41:17 -0800

    nice list of books

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -2,18 +2,15 @@ from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre
 
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 admin.site.register(Author, AuthorAdmin)
-
-
-# admin.site.register(Book)
-# admin.site.register(BookInstance)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'display_genre')
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     pass
+
 admin.site.register(Genre)
