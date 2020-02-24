a963438eaa6183385299f12c896e76891c214dd2
arsummers <aliyasummers1@gmail.com>
2020-02-24 14:31:39 -0800

    super user created

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(BookInstance)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Author, AuthorAdmin)
+
+
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
 admin.site.register(Genre)
