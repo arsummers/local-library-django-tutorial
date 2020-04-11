20b5f223189b5947e5f06303028f76986aed4024
arsummers <aliyasummers1@gmail.com>
2020-04-10 17:34:38 -0700

    forms section finished

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 from catalog.models import Book, BookInstance, Author, Genre
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 import datetime
 
 from catalog.forms import RenewBookForm
+from catalog.models import Author
 
 def index(request):
     """View function for site homepage"""
@@ -83,3 +85,16 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death': '05/01/2018'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
